b1d8fac443d38c9219fae1c4f68f2fbd6d0a8107
Kevin Ruiz <kmedina@petsy.mx>
2020-01-09 15:50:47 -0600

    add regiter to observer

--- Observer/SalesOrderSaveAfterObserver.php
+++ Observer/SalesOrderSaveAfterObserver.php
@@ -8,9 +8,11 @@ class SalesOrderSaveAfterObserver implements ObserverInterface {
 	protected $_logger;
 
 	public function __construct(
-		\Psr\Log\LoggerInterface $logger
+		\Psr\Log\LoggerInterface $logger,
+		\Magento\Framework\Registry $registry
 	) {
 		$this->_logger = $logger;
+		$this->registry = $registry;
 		// Observer initialization code...
 		// You can use dependency injection to get any class this observer may need.
 	}
@@ -18,7 +20,13 @@ class SalesOrderSaveAfterObserver implements ObserverInterface {
 	public function execute(\Magento\Framework\Event\Observer $observer) {
 		$order = $observer->getOrder();
 		$method = $order->getShippingMethod(); //envioskanguro_standar_223
-		$this->_logger->info(' |->>> ', explode("_", $method));
+        $rates = $this->>_registry->registry('Rates');
+        $data = $rates['body']->data;
+        foreach ($$data as $key => $rate) {
+        	# code...
+			$this->_logger->info(' |->>> '.$rate, true);
+        }
+
 		// Additional observer execution code...
 		return $this;
 	}
